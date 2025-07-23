73b5996e816f80218251b65e0650894327029722
Tommus129 <tommasoprandini2001@gmail.com>
2025-07-23 19:31:48 +0200

    Aggiunto progetto completo

--- Play/start.bat
+++ Play/start.bat
@@ -1,17 +1,16 @@
 @echo off
 cd /d "%~dp0"
-
-set "JAVA_BIN=custom-jre\bin\java.exe"
+set "JAVA_BIN=custom-jre-win\bin\java.exe"
 set "JAR_FILE=play.jar"
 
 if not exist "%JAVA_BIN%" (
-    echo ERRORE: Java non trovato in %JAVA_BIN%
+    echo ERRORE: file java.exe non trovato in %JAVA_BIN%
     pause
     exit /b
 )
 
 if not exist "%JAR_FILE%" (
-    echo ERRORE: File %JAR_FILE% non trovato
+    echo ERRORE: file %JAR_FILE% non trovato
     pause
     exit /b
 )
